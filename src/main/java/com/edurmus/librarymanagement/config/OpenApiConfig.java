c6db0536721e01e71107e922cccc55f05b787810
emred <emredurmus0@hotmail.com>
2025-05-13 18:12:12 +0300

    feat: Add OpenAPI JWT security configuration and Swagger UI support

--- src/main/java/com/edurmus/librarymanagement/config/OpenApiConfig.java
+++ src/main/java/com/edurmus/librarymanagement/config/OpenApiConfig.java
@@ -1,23 +1,43 @@
 package com.edurmus.librarymanagement.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Library Management API",
+                version = "1.0.0",
+                description = "API for managing library resources",
+                contact = @Contact(name = "Emre Durmus", email = "edurrmus@gmail.com")
+        ),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
+                .info(new io.swagger.v3.oas.models.info.Info()
                         .title("Library Management API")
                         .description("API for managing library resources")
                         .version("1.0.0")
-                        .contact(new Contact()
+                        .contact(new io.swagger.v3.oas.models.info.Contact()
                                 .name("Emre Durmus")
                                 .email("edurrmus@gmail.com")));
     }
